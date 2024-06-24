464d3e357ec0b9401f4db1e844f866b37970de4a
Pradipkakde <kakde.pradip1@gmail.com>
2024-06-24 21:46:47 +0530

    Basic setup is ready of Ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactManagerComponent } from './Component/contact-manager/contact-manager.component';
+import { AddContactComponent } from './Component/add-contact/add-contact.component';
+import { EditContactComponent } from './Component/edit-contact/edit-contact.component';
+import { ViewContactComponent } from './Component/view-contact/view-contact.component';
+import { PageNotFoundComponent } from './Component/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/contacts/admin',pathMatch:'full'},
+  {path:'contacts/admin',component:ContactManagerComponent},
+  {path:'contacts/add',component:AddContactComponent},
+  {path:'contacts/edit/:contactId',component:EditContactComponent},
+  {path:'contacts/view/:contactid',component:ViewContactComponent},
+  {path:'**',component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Component/navbar/navbar.component';
+import { ContactManagerComponent } from './Component/contact-manager/contact-manager.component';
+import { AddContactComponent } from './Component/add-contact/add-contact.component';
+import { EditContactComponent } from './Component/edit-contact/edit-contact.component';
+import { ViewContactComponent } from './Component/view-contact/view-contact.component';
+import { SpinnerComponent } from './Component/spinner/spinner.component';
+import { PageNotFoundComponent } from './Component/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactManagerComponent,
+    AddContactComponent,
+    EditContactComponent,
+    ViewContactComponent,
+    SpinnerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
