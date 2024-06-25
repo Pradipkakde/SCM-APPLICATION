a694ea7995f78129c5f6f6b2703145c18b229000
Pradipkakde <kakde.pradip1@gmail.com>
2024-06-25 21:34:04 +0530

    ready error message and spinner

--- src/app/Component/contact-manager/contact-manager.component.ts
+++ src/app/Component/contact-manager/contact-manager.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IContact } from 'src/app/Models/IContact';
+import { ContactService } from 'src/app/Service/contact.service';
 
 @Component({
   selector: 'app-contact-manager',
   templateUrl: './contact-manager.component.html',
   styleUrls: ['./contact-manager.component.css']
 })
-export class ContactManagerComponent {
+export class ContactManagerComponent implements OnInit {
 
+  public loading:boolean=false;
+  public contacts:IContact[]=[];
+  public errorMessage:string|null=null;
+
+ constructor(private contactService:ContactService){}
+  ngOnInit(): void {
+    this.loading=true;
+    this.contactService.getAllcontacts().subscribe((data:IContact[])=>{
+      this.contacts=data;
+      this.loading=false;
+
+    
+  },(error)=>{
+    this.errorMessage=error;
+    this.loading=false;
+
+  });
+
+  }
 }
--- src/app/Service/contact.service.ts
+++ src/app/Service/contact.service.ts
@@ -1,9 +1,71 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { IContact } from '../Models/IContact';
+import { IGroup } from '../Models/IGroup';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
-}
+  private  serverUrl:string=`http://localhost:3000`;
+
+  constructor(private httpclinet:HttpClient) { }
+
+//Get all contacts
+  public getAllcontacts():Observable<IContact[]>{
+    let dataURL:string=`${this.serverUrl}/contacts`;
+    return this.httpclinet.get<IContact[]>(dataURL).pipe(catchError(this.handleError)); 
+  }
+
+  //Get single contacts
+  public getContact(contactId:string):Observable<IContact>{
+    let dataURL:string=`${this.serverUrl}/contacts/${contactId}`;
+    return this.httpclinet.get<IContact>(dataURL).pipe(catchError(this.handleError));
+  }
+  //create a contact
+  public createContact(contact:IContact):Observable<IContact>{
+    let dataURL:string=`${this.serverUrl}/contacts`;
+    return this.httpclinet.post<IContact>(dataURL,contact).pipe(catchError(this.handleError));    
+  }
+  //update a contact
+  public updateContact(contact:IContact,contactId:string):Observable<IContact>{
+    let dataURL:string=`${this.serverUrl}/contacts/${contact.id}`;
+    return this.httpclinet.put<IContact>(dataURL,contact).pipe(catchError(this.handleError));
+  }
+
+  //delete a contact
+  public deleteContact(contactId:string):Observable<{}>{
+    let dataURL:string=`${this.serverUrl}/contacts/${contactId}`;
+    return this.httpclinet.delete<{}>(dataURL).pipe(catchError(this.handleError));
+  }
+
+  //All Groups
+  public getAllGroups():Observable<IGroup[]>{
+    let dataURL:string=`${this.serverUrl}/groups`;
+    return this.httpclinet.get<IGroup[]>(dataURL).pipe(catchError(this.handleError)); 
+  }
+  //Get single contacts
+  public getGroup(contact:IContact):Observable<IGroup>{
+    let dataURL:string=`${this.serverUrl}/groups/${contact.groupId}`;
+    return this.httpclinet.get<IGroup>(dataURL).pipe(catchError(this.handleError));
+  }
+
+
+
+  //error handler
+public handleError(error:HttpErrorResponse){
+  let errorMessage:string='';
+  if(error.error instanceof ErrorEvent){
+    errorMessage=`Error :${error.error.message}`
+  }
+  else{
+    errorMessage=`Status:${error.status}\n Message:${error.message} `;
+  }
+  return throwError(errorMessage);
+  
+  }
+  
+  }
+
